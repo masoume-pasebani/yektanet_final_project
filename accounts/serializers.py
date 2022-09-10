85e8b47685e4ec5dec39f82d458e2c995a50dab1
e <mwsmee@gmail.com>
2022-09-10 18:11:59 +0430

    serializer finished.

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -9,11 +9,13 @@ from accounts.models import Applicant, Interviewer, HR
 class ApplicantSignUpSerializer(serializers.ModelSerializer):
     username = serializers.CharField(max_length=100, write_only=True)
     password = serializers.CharField(max_length=100, write_only=True, style={'input_type': 'password', 'placeholder': 'Password'})
+    email = serializers.EmailField(max_length=100)
 
     def save(self, **kwargs):
         user_model: User = get_user_model()
         username = self.validated_data.pop('username')
         password = self.validated_data.pop('password')
+        email = self.validated_data.pop('email')
         user = user_model.objects.create_user(username=username, password=password)
 
         Token.objects.create(user=user)
@@ -25,7 +27,7 @@ class ApplicantSignUpSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Applicant
-        fields = ('id', 'username', 'password', 'first_name', 'last_name', 'resume', 'linkedin', 'age', 'gender', 'status')
+        fields = ('id', 'username', 'password','email', 'first_name', 'last_name', 'resume', 'linkedin', 'age', 'gender', 'status')
 
     def to_representation(self, instance):
         return ApplicantSerializer(instance).data
@@ -42,13 +44,15 @@ class ApplicantSerializer(serializers.ModelSerializer):
 class InterviewerSerializer(serializers.ModelSerializer):
     username = serializers.CharField(max_length=100, write_only=True)
     password = serializers.CharField(max_length=100, write_only=True, style={'input_type': 'password', 'placeholder': 'Password'})
+    email = serializers.EmailField(max_length=200)
 
     def save(self, **kwargs):
 
         user_model: User = get_user_model()
         username = self.validated_data.pop('username')
         password = self.validated_data.pop('password')
-        user = user_model.objects.create_user(username=username, password=password)
+        email = self.validated_data.pop('email')
+        user = user_model.objects.create_user(username=username, password=password, email=email)
 
         Token.objects.create(user=user)
         interviewer = super().save(user=user)
@@ -58,19 +62,21 @@ class InterviewerSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Interviewer
-        fields = ('username', 'password', 'first_name', 'last_name')
+        fields = ('username', 'password', 'email', 'first_name', 'last_name')
 
 
 class HRSerializer(serializers.ModelSerializer):
     username = serializers.CharField(max_length=100, write_only=True)
     password = serializers.CharField(max_length=100, write_only=True, style={'input_type': 'password', 'placeholder': 'Password'})
+    email = serializers.EmailField(max_length=200)
 
     def save(self, **kwargs):
 
         user_model: User = get_user_model()
         username = self.validated_data.pop('username')
         password = self.validated_data.pop('password')
-        user = user_model.objects.create_user(username=username, password=password)
+        email = self.validated_data.pop('email')
+        user = user_model.objects.create_user(username=username, password=password, email= email)
         user.is_staff = True
         user.is_superuser = True
         Token.objects.create(user=user)
@@ -82,4 +88,4 @@ class HRSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = HR
-        fields = ('username', 'password', 'first_name', 'last_name', 'phone_number')
+        fields = ('username', 'password', 'email', 'first_name', 'last_name', 'phone_number')
